e147c901421cb03410d29ac58ece8505a329c675
Lalitha-ganti <135207124+Lalitha-ganti@users.noreply.github.com>
2023-06-10 01:27:40 +0530

    Add files via upload

---
+++ Java/Activities/Activity9.java
@@ -0,0 +1,28 @@
+package Activities;
+import java.util.ArrayList;
+public class Activity9 {
+    public static void main(String[] args) {
+        //declaring Arraylist of String objects
+        ArrayList<String> myList = new ArrayList<String>();
+        //Adding objects to Array List at default index
+        myList.add("Ken");
+        myList.add("Rom");
+        myList.add("Bob");
+        //Adding object at specific index
+        myList.add(3, "Tiny");
+        myList.add(1, "Papaya");
+
+        System.out.println("Print All the Objects:");
+        for(String s:myList){
+            System.out.println(s);
+        }
+
+        System.out.println("3rd element in the list is: " + myList.get(2));
+        System.out.println("Is Mini is in list: " + myList.contains("Mini"));
+        System.out.println("Size of ArrayList: " + myList.size());
+
+        myList.remove("Papaya");
+
+        System.out.println("New Size of ArrayList: " + myList.size());
+    }
+}
